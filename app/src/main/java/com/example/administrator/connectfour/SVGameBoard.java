20a3bb5f84d66d16dfc64c5ba457f45b12e189ef
garciah16 <garciah16@up.edu>
2015-10-03 16:10:55 -0700

    added an options activity, modified the SVGameBoard to be editable, hopefully

--- app/src/main/java/com/example/administrator/connectfour/SVGameBoard.java
+++ app/src/main/java/com/example/administrator/connectfour/SVGameBoard.java
@@ -7,6 +7,7 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.SurfaceView;
+
 import static android.graphics.Color.*;
 
 
@@ -19,10 +20,13 @@ public class SVGameBoard extends SurfaceView {
     public static final int RADIUS = 80;
     public static final int OFFSET = 85;
 
-    private int[] freeSpaces = {0,0,0,0,0,0}; //array of all free spaces in column
-    public static final int FREE =0;
+    private int[] freeSpaces = {0, 0, 0, 0, 0, 0}; //array of all free spaces in column
+    public static final int FREE = 0;
+    public static final int TAKEN = 1;
     public static final int RED = 2;
-    public static final int YELLOW =3;
+    public static final int YELLOW = 3;
+
+    protected Canvas columnCanvas;
 
     public SVGameBoard(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -31,8 +35,11 @@ public class SVGameBoard extends SurfaceView {
 
 
     @Override
-   protected void onDraw(Canvas canvas) {
+    protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        //save the canvas for future use. this will allow us to edit
+        columnCanvas = canvas;
         Paint blue = new Paint();
         blue.setColor(BLUE);
         Rect gameBoard = new Rect();
@@ -44,36 +51,27 @@ public class SVGameBoard extends SurfaceView {
         for (int i = 1; i < 7; i++) {
             canvas.drawCircle(OFFSET, OFFSET * 2 * i, RADIUS, white);
         }
-
-        //the below is only to make an example of what the board will look like in play
-        //this should be removed later as this is not how we will make the in play board function
-        //(unless overlapping shapes turns out to work better than changing colors
-//        Paint red = new Paint();
-//        red.setColor(Color.RED);
-//        Paint yellow = new Paint();
-//        yellow.setColor(Color.YELLOW);
-//        canvas.drawCircle(OFFSET, OFFSET * 2 * 6, RADIUS, red);
-//        canvas.drawCircle(OFFSET, OFFSET * 2 * 5, RADIUS, yellow);
-
     }
 
-    public boolean addToken(int color){
-        for(int i=5; i == 0 ;i--){
-            if(this.freeSpaces[i] == FREE){
+    public int addToken(int color) {
+        //loop through all free spaces until you find one that is empty
+        for (int i = 5; i == 0; i--) {
+            if (this.freeSpaces[i] == FREE) {
+                //this will identify a space as color
                 this.freeSpaces[i] = color;
 
-                return true;
+                return i;
             }
         }
-        return false; //if all spaces are taken, colum full
+        return -1; //if all spaces are taken, column full
     }
     //TODO: make columns different from each other
- //   protected void drawColumn(Canvas canvas, int id){
+    //   protected void drawColumn(Canvas canvas, int id){
 
- //       Paint red = new Paint()
- //     red.setColor(Color.RED);
- //       if()
- //   }
+    //       Paint red = new Paint()
+    //     red.setColor(Color.RED);
+    //       if()
+    //   }
 
 }
 
